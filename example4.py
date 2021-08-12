6361e23cd615ac014be48dd198caaadccd3ed011
Jon Mockbee <mockbee1000@gmail.com>
2021-08-12 16:22:51 -0400

    this is an inifinite loop

---
+++ example4.py
@@ -0,0 +1,11 @@
+def print_range(start, end):
+	# Loop through the numbers from start to end
+	n = start 
+	while (n <= end):
+            n=n+1
+	    print(n)
+	 
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line) 
+        
+    
+
