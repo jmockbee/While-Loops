f4df795a1cf750a0d4c3c981a1e3864cc53e2d9f
Jon Mockbee <mockbee1000@gmail.com>
2021-08-14 20:20:34 -0400

    another one down

--- question4.py
+++ question4.py
@@ -2,13 +2,12 @@ def sum_divisors(n):
     sum = 0 
     x = 1
     while n !=0 and x < n:
-        if n % x == 0
-            sum += x
-        
-
+            if n % x == 0:
+                sum += x
+            else:
+                sum += 0
+            x += 1  
     
-
-
 
 
             return sum
